8f83d4d8e35135d2b5b822f045ef22d2c63fe88c
Brad Madigan <bradmadigan@gmail.com>
2019-12-06 18:32:08 -0500

    Setup the design for Dashboar TopNav

--- resources/js/Shared/Layouts/Dashboard.vue
+++ resources/js/Shared/Layouts/Dashboard.vue
@@ -57,9 +57,51 @@
         </section>
 
         <main class="w-full">
-            <div class="bg-indigo-900 text-indigo-600">
-                I Am A TopBar
+            <div class="bg-indigo-900 h-48 px-8 pt-4">
+                <div class="flex justify-between items-center">
+                    <div class="flex items-center">
+                        <img src="/svgs/wordmark-color.svg" class="w-8 mr-3" />
+                        <h2 class="text-white text-3xl font-semibold">Account Summary</h2>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="mr-2">
+                            <img
+                                src="https://randomuser.me/api/portraits/women/65.jpg"
+                                class="w-8 rounded-full border-indigo-200 border-2"
+                            />
+                        </div>
+                        <div class="text-white text-sm">
+                            Brad madigan
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <div class="-mt-32 flex justify-between px-8">
+                <div class="mt-6 px-6 py-6 w-3/5 bg-white rounded-lg shadow flex justify-between">
+                    <div>
+                        <h3 class="text-gray-700 text-lg font-semibold pb-2">Brad and Karen Madigan</h3>
+                        <div class="text-gray-500 text-sm">
+                            30 Texas Ave,
+                            <br />
+                            Sault Ste. Marie, Ontario
+                            <br />
+                            CA P6A 4Y8
+                        </div>
+                    </div>
+                    <div>
+                        <button class="bg-indigo-100 text-sm text-indigo-700 rounded px-4 py-2">
+                            Update Address
+                        </button>
+                    </div>
+                </div>
+
+                <div class="text-right w-2/5 mt-3">
+                    <div class="uppercase text-sm text-indigo-500 pb-1">Total Balance</div>
+                    <div class="text-4xl text-white font-semibold">$54,023.33</div>
+                </div>
+            </div>
+
             <slot />
         </main>
     </div>
