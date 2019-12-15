591f4b0b07e5482e7ed01a0104085bcb8be60e91
Brad Madigan <bradmadigan@gmail.com>
2019-12-15 11:13:01 -0500

    add subtract money modal

--- resources/js/Pages/Accounts/Show.vue
+++ resources/js/Pages/Accounts/Show.vue
@@ -1,36 +1,56 @@
 <template>
     <app-layout>
-            <div class="bg-white px-8 pt-4">
-                <div class="flex justify-between items-center">
-                    <div class="flex items-center">
-                        <img src="/svgs/wordmark-color.svg" class="w-8 mr-3" />
-                        <h2 class="text-gray-800 text-3xl font-semibold">Account Activity</h2>
-                    </div>
-                    <profile-dropdown :user="$page.auth.user" theme="app"></profile-dropdown>
+        <div class="bg-white px-8 pt-4">
+            <div class="flex justify-between items-center">
+                <div class="flex items-center">
+                    <img src="/svgs/wordmark-color.svg" class="w-8 mr-3" />
+                    <h2 class="text-gray-800 text-3xl font-semibold">Account Activity</h2>
                 </div>
-
-                <account-activity-top-bar :account="account" :type="accountType" />
+                <profile-dropdown :user="$page.auth.user" theme="app"></profile-dropdown>
             </div>
+
+            <account-activity-top-bar :account="account" :type="accountType" />
+        </div>
 
         <div class="px-8 mt-10">
             <div class="flex items-center justify-between mt-12 mb-10">
                 <h2 class="text-2xl text-gray-600 font-semibold">Latest Transactions</h2>
                 <div class="flex items-center">
-                    <button class="flex items-center btn-sm btn-pink mr-4">
-                        <svg  viewBox="0 0 24 24" class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-                            <defs/>
-                            <path fill-rule="evenodd" d="M6.586 12.828L4.41 15.004c-.58.58-1.59.996-2.409.996H0v-2h2.001c.29 0 .792-.207.995-.41l2.176-2.176 1.414 1.414zM16 6h-2c-.29 0-.794.209-.996.41l-2.176 2.176-1.414-1.414 2.176-2.176C12.167 4.42 13.18 4 14 4h2V1l4 4-4 4V6zm0 10h-2c-.82 0-1.833-.42-2.41-.996L2.996 6.41C2.793 6.207 2.29 6 2.001 6H0V4h2.001c.819 0 1.83.417 2.41.996l8.593 8.594c.202.201.706.41.996.41h2v-3l4 4-4 4v-3z"/>
+                    <button
+                        @click.prevent="$modal.show('subtract-money-modal')"
+                        class="flex items-center btn-sm btn-pink mr-4"
+                    >
+                        <svg
+                            viewBox="0 0 24 24"
+                            class="fill-current w-4 h-4 mr-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                        >
+                            <defs />
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 20c5.523 0 10-4.477 10-10S15.523 0 10 0 0 4.477 0 10s4.477 10 10 10zm5-11v2H5V9h10z"
+                            />
                         </svg>
-                            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" fill-rule="evenodd" />
-                        </svg>
-                        <span>Transfer Money</span>
+                        <span>Withdraw Money</span>
                     </button>
 
                     <button
                         @click.prevent="$modal.show('add-money-modal')"
-                        class="flex items-center btn-sm btn-pink mr-4">
-                        <svg viewBox="0 0 24 24" class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-                            <path fill-rule="evenodd" d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11c5.523 0 10-4.477 10-10S15.523 0 10 0 0 4.477 0 10s4.477 10 10 10z"/>
+                        class="flex items-center btn-sm btn-green mr-4"
+                    >
+                        <svg
+                            viewBox="0 0 24 24"
+                            class="fill-current w-4 h-4 mr-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11c5.523 0 10-4.477 10-10S15.523 0 10 0 0 4.477 0 10s4.477 10 10 10z"
+                            />
                         </svg>
                         <span>Add Money</span>
                     </button>
@@ -71,6 +91,8 @@
         </div>
 
         <add-money-modal :account="account" />
+
+        <subtract-money-modal :account="account" />
     </app-layout>
 </template>
 
@@ -78,17 +100,19 @@
 import AppLayout from "@/Shared/Layouts/App";
 import ProfileDropdown from "@/Shared/ProfileDropdown";
 import AddMoneyModal from "./AddMoneyModal";
+import SubtractMoneyModal from "./SubtractMoneyModal";
 import AccountActivityTopBar from "@/components/Accounts/AccountActivityTopBar";
 import DataTable from "@/components/DataTables/DataTable";
 import DataTableHeader from "@/components/DataTables/DataTableHeader";
 import DataTableRow from "@/components/DataTables/DataTableRow";
 
 export default {
-    props: ['account', 'accountType', 'transactions'],
+    props: ["account", "accountType", "transactions"],
     components: {
         AppLayout,
         ProfileDropdown,
         AddMoneyModal,
+        SubtractMoneyModal,
         AccountActivityTopBar,
         DataTable,
         DataTableHeader,
@@ -107,6 +131,6 @@ export default {
                 { title: "Balance", key: "balance" }
             ];
         }
-    },
+    }
 };
 </script>
---
+++ resources/js/Pages/Accounts/SubtractMoneyModal.vue
@@ -0,0 +1,103 @@
+<template>
+    <modal
+        name="subtract-money-modal"
+        :draggable="false"
+        :scrollable="false"
+        :height="280"
+        :width="400"
+        :clickToClose="false"
+        classes="modal-container"
+    >
+        <div class="modal-content">
+            <form @submit.prevent="submitForm">
+                <div class="modal-body ">
+                    <div class="flex items-center justify-between border-b border-gray-200 py-6 px-4">
+                        <div class="flex items-center">
+                            <img src="/svgs/wordmark-color.svg" class="w-8 mr-3" />
+                            <h2 class="text-gray-800 font-semibold text-xl">Withdraw Money</h2>
+                        </div>
+                        <svg
+                            @click="cancel"
+                            viewBox="0 0 24 24"
+                            class="fill-current text-gray-500 w-6 h-6 cursor-pointer"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                        >
+                            <defs />
+                            <path
+                                fill-rule="evenodd"
+                                d="M11.414 10l2.829-2.828-1.415-1.415L10 8.586 7.172 5.757 5.757 7.172 8.586 10l-2.829 2.828 1.415 1.415L10 11.414l2.828 2.829 1.415-1.415L11.414 10zM2.93 17.071c3.905 3.905 10.237 3.905 14.142 0 3.905-3.905 3.905-10.237 0-14.142-3.905-3.905-10.237-3.905-14.142 0-3.905 3.905-3.905 10.237 0 14.142z"
+                            />
+                        </svg>
+                    </div>
+
+                    <div class="mt-6 justify-center w-full">
+                        <div class="pb-4 px-4 flex justify-between items-center">
+                            <label class="block">
+                                <span class="text-gray-700 w-full">Withdraw Amount</span>
+                                <input
+                                    v-model="form.withdraw_amount"
+                                    v-money="money"
+                                    class="form-input mt-1 block w-full"
+                                    placeholder="$0.00"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer flex items-center justify-between">
+                    <p class="p-6 cursor-pointer" @click="cancel">Cancel</p>
+                    <div class="p-6 text-right">
+                        <button
+                            type="submit"
+                            name="subtractMoney"
+                            class="bg-indigo-700 hover:bg-indigo-600 text-white rounded text-xs px-4 py-2"
+                        >
+                            Withdraw
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </modal>
+</template>
+
+<script>
+export default {
+    props: ["account"],
+    data() {
+        return {
+            form: {
+                account_uuid: this.account.uuid,
+                withdraw_amount: 0.0
+            },
+            money: {
+                decimal: ".",
+                thousands: ",",
+                prefix: "$ ",
+                precision: 2,
+                masked: false /* doesn't work with directive */
+            }
+        };
+    },
+    methods: {
+        submitForm() {
+            this.$inertia.patch(route("accounts.update", this.account.uuid), this.form).then(() => {
+                this.resetForm();
+                this.close();
+            });
+        },
+        cancel() {
+            this.resetForm();
+            this.close();
+        },
+        resetForm() {
+            this.form.withdraw_amount = 0.0;
+        },
+        close() {
+            this.$modal.hide("subtract-money-modal");
+        }
+    }
+};
+</script>
