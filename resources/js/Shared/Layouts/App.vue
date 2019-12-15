2caffc5eb99592213c2082ea90304e9df664cb7f
Brad Madigan <bradmadigan@gmail.com>
2019-12-15 06:56:42 -0500

    add toast notifications to layouts

--- resources/js/Shared/Layouts/App.vue
+++ resources/js/Shared/Layouts/App.vue
@@ -35,6 +35,83 @@ import Sidenav from "@/Shared/Sidenav";
 import AccountActivityTopBar from "@/Shared/AccountActivityTopBar";
 
 export default {
-    components: { Sidenav, AccountActivityTopBar }
+    components: { Sidenav, AccountActivityTopBar },
+    methods: {
+        showFlashMessage() {
+            return this.$toasted.show(this.$page.flash.success, {
+                position: "bottom-right",
+                duration: 4000,
+                keepOnHover: true,
+                type: "success",
+                theme: "bubble",
+                action: {
+                    onClick: (e, toastObject) => {
+                        toastObject.goAway(0);
+                    }
+                }
+            });
+        },
+        showErrorMessages() {
+            if (this.$page.errors) {
+                const options = {
+                    position: "bottom-right",
+                    duration: 4000,
+                    keepOnHover: true,
+                    type: "error",
+                    theme: "bubble",
+                    action: {
+                        onClick: (e, toastObject) => {
+                            toastObject.goAway(0);
+                        }
+                    }
+                };
+                var errors = this.$page.errors;
+                var messages = Object.values(errors);
+                var that = this;
+                messages.forEach(function(msg) {
+                    that.$toasted.show(msg, options);
+                });
+            }
+        },
+        showAnErrorMessage() {
+            return this.$toasted.show(this.$page.flash.error, {
+                position: "bottom-right",
+                duration: 4000,
+                keepOnHover: true,
+                type: "error",
+                theme: "bubble",
+                action: {
+                    onClick: (e, toastObject) => {
+                        toastObject.goAway(0);
+                    }
+                }
+            });
+        }
+    },
+    watch: {
+        "$page.flash": {
+            handler() {
+                this.showFlashMessage();
+            },
+            deep: true
+        },
+        "$page.errors": {
+            handler() {
+                this.showErrorMessages();
+            },
+            deep: true
+        },
+        "$page.error": {
+            handler() {
+                this.showAnErrorMessage();
+            },
+            deep: true
+        }
+    },
+    mounted() {
+        this.showFlashMessage();
+        this.showErrorMessages();
+        this.showAnErrorMessage();
+    }
 };
 </script>
--- resources/js/Shared/Layouts/Dashboard.vue
+++ resources/js/Shared/Layouts/Dashboard.vue
@@ -15,6 +15,83 @@ import Sidenav from "@/Shared/Sidenav";
 import TopnavDashboard from "@/Shared/TopnavDashboard";
 
 export default {
-    components: { Sidenav, TopnavDashboard }
+    components: { Sidenav, TopnavDashboard },
+    methods: {
+        showFlashMessage() {
+            return this.$toasted.show(this.$page.flash.success, {
+                position: "bottom-right",
+                duration: 4000,
+                keepOnHover: true,
+                type: "success",
+                theme: "bubble",
+                action: {
+                    onClick: (e, toastObject) => {
+                        toastObject.goAway(0);
+                    }
+                }
+            });
+        },
+        showErrorMessages() {
+            if (this.$page.errors) {
+                const options = {
+                    position: "bottom-right",
+                    duration: 4000,
+                    keepOnHover: true,
+                    type: "error",
+                    theme: "bubble",
+                    action: {
+                        onClick: (e, toastObject) => {
+                            toastObject.goAway(0);
+                        }
+                    }
+                };
+                var errors = this.$page.errors;
+                var messages = Object.values(errors);
+                var that = this;
+                messages.forEach(function(msg) {
+                    that.$toasted.show(msg, options);
+                });
+            }
+        },
+        showAnErrorMessage() {
+            return this.$toasted.show(this.$page.flash.error, {
+                position: "bottom-right",
+                duration: 4000,
+                keepOnHover: true,
+                type: "error",
+                theme: "bubble",
+                action: {
+                    onClick: (e, toastObject) => {
+                        toastObject.goAway(0);
+                    }
+                }
+            });
+        }
+    },
+    watch: {
+        "$page.flash": {
+            handler() {
+                this.showFlashMessage();
+            },
+            deep: true
+        },
+        "$page.errors": {
+            handler() {
+                this.showErrorMessages();
+            },
+            deep: true
+        },
+        "$page.error": {
+            handler() {
+                this.showAnErrorMessage();
+            },
+            deep: true
+        }
+    },
+    mounted() {
+        this.showFlashMessage();
+        this.showErrorMessages();
+        this.showAnErrorMessage();
+    }
 };
 </script>
