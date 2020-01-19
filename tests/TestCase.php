9b46172149f6241e735cf6f361f4d50713a3b3f9
Brad Madigan <bradmadigan@gmail.com>
2020-01-19 10:45:13 -0500

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Domain\Users\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function signIn($user = null, $attributes = [])
+    {
+        $user = $user ?: factory(User::class)->create($attributes);
+        $this->actingAs($user);
+
+        return $this;
+    }
 }
